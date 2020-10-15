6faeced9455b47bff8a75709fbd2e0de5971567c
Wiktor Czajkowski <malyczaja12@gmail.com>
2020-10-15 13:39:10 +0200

    fixed bug with nav in mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 const navMobile = document.querySelector('.nav-mobile');
+const allNavMobile = document.querySelectorAll('.nav__link-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
-
-
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
+
+    if (navBtn.classList.contains('is-active')) {
+        navBtn.style.position = 'fixed';
+    } else {
+        navBtn.style.position = 'absolute';
+    }
+
+    allNavMobile.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile--active');
+            navBtn.classList.remove('is-active');
+            navBtn.style.position = 'absolute';
+        });
+    });
 
 }
 
